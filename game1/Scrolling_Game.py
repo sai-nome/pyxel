420fab0f486b16af3022303c26988dee5d44352d
sai-nome <beeskynohito@gmail.com>
2025-03-02 14:16:02 +1300

    fix: code refactoring

--- game1/Scrolling_Game.py
+++ game1/Scrolling_Game.py
@@ -28,38 +28,34 @@ class Scrolling_Game:
         pyxel.playm(0, loop=True)
         pyxel.run(self.update, self.draw)
 
-    def update(self):
-        # ゲーム中断
-        if pyxel.btnp(pyxel.KEY_Q):
-            pyxel.quit()
-        # Rキーが押されたらリセット
-        if pyxel.btnp(pyxel.KEY_R):
-            self.char_x = 50  # キャラクターのX座標
-            self.char_y = 50  # キャラクターのY座標
-            self.char_frame = 0  # 表示する画像のインデックス (0 または 1)
-            self.frame_count = 0 # フレームカウンター
-            self.scroll_x = 0 # スクロール位置
-            self.scroll_speed = 1 # スクロール速度
-            self.is_moving = False  # 移動中フラグを追加
-            self.last_input = False #直前のフレームでキー入力があったか
-            self.item_x = random.randint(0, 120)
-            self.item_y = random.randint(0, 120)
-            self.spiderweb_x = 128
-            self.spiderweb_y = random.randint(0, 120)
-            self.spiderweb_speed = 3
-            self.sickle_x = 128
-            self.sickle_y = random.randint(0, 120)
-            self.sickle_speed = 3
-            self.score = 0
-            self.is_out_of_screen = False  # 画面外フラグ
-            self.collision_flag = False
+    def reset_game(self):
+        self.char_x = 50
+        self.char_y = 50
+        self.char_frame = 0
+        self.frame_count = 0
+        self.scroll_x = 0
+        self.scroll_speed = 1
+        self.is_moving = False
+        self.last_input = False
+        self.item_x = random.randint(0, 120)
+        self.item_y = random.randint(0, 120)
+        self.spiderweb_x = 128
+        self.spiderweb_y = random.randint(0, 120)
+        self.spiderweb_speed = 3
+        self.sickle_x = 128
+        self.sickle_y = random.randint(0, 120)
+        self.sickle_speed = 3
+        self.score = 0
+        self.is_out_of_screen = False
+        self.collision_flag = False
 
-        self.scroll_x += self.scroll_speed  # スクロール位置を更新
-        self.char_x -= self.scroll_speed  # キャラ位置を更新
-        # スクロール位置が背景画像の幅を超えたら、0に戻す
-        if self.scroll_x > 128:  # 128は背景画像の幅
-            self.scroll_x = 0 
+    def update_scroll(self):
+        self.scroll_x += self.scroll_speed
+        self.char_x -= self.scroll_speed
+        if self.scroll_x > 128:
+            self.scroll_x = 0
 
+    def update_obstacles(self):
         self.spiderweb_x -= self.spiderweb_speed
         self.sickle_x -= self.sickle_speed
         if self.spiderweb_x < 0:
@@ -68,32 +64,26 @@ class Scrolling_Game:
         elif self.sickle_x < 0:
             self.sickle_x = 128
             self.sickle_y = random.randint(0, 120)
-        # 当たり判定
+
+    def check_collision(self):
         if (self.char_x < self.spiderweb_x + 12 and
             self.char_x + 12 > self.spiderweb_x and
             self.char_y < self.spiderweb_y + 12 and
             self.char_y + 12 > self.spiderweb_y):
-
-            # 当たり時の処理 (例: ゲームオーバー)
             self.collision_flag = True
 
-        # 当たり判定
         if (self.char_x < self.sickle_x + 12 and
             self.char_x + 12 > self.sickle_x and
             self.char_y < self.sickle_y + 12 and
             self.char_y + 12 > self.sickle_y):
-
-            # 当たり時の処理 (例: ゲームオーバー)
             self.collision_flag = True
 
-        # キー入力と移動処理
+    def handle_input(self):
         self.is_moving = False
-        key_pressed = False # 今のフレームでキーが押されているか
-
-        # キー入力処理とキャラクターの移動
+        key_pressed = False
         if pyxel.btn(pyxel.KEY_LEFT):
             self.char_x -= 1
-            self.is_moving = True  # 移動中フラグを立てる
+            self.is_moving = True
             key_pressed = True
         if pyxel.btn(pyxel.KEY_RIGHT):
             self.char_x += 3
@@ -113,11 +103,21 @@ class Scrolling_Game:
             if self.frame_count % 10 == 0:
                 self.char_frame = 1 - self.char_frame
         elif self.is_moving and not self.last_input:
-                self.frame_count = 0
-        
-        self.last_input = key_pressed # 次のフレームのためにキー入力があったかを保存
+            self.frame_count = 0
 
-        # 当たり判定
+        self.last_input = key_pressed
+
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+        if pyxel.btnp(pyxel.KEY_R):
+            self.reset_game()
+
+        self.update_scroll()
+        self.update_obstacles()
+        self.check_collision()
+        self.handle_input()
+
         if (self.char_x < self.item_x + 8 and 
             self.char_x + 16 > self.item_x and
             self.char_y < self.item_y + 8 and
